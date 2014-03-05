e3e3e90ab8ebefbd87a63f10b271557ebce73eea
Thomas de Grivel <thomas@lowh.net>
2014-03-05 15:01:29 +0100

    Wiki permissions = anonymous read only

--- app/controllers/wiki-controller.lisp
+++ app/controllers/wiki-controller.lisp
@@ -1,15 +1,20 @@
 
 (defun /wiki#show (article)
-  (template-let (article)
-    (render-view :wiki :show '.html)))
+  (template-let (article
+		 (id (str "wiki_" (to-url (article.name article)))))
+    (render-view :wiki (if (session-user) :show-editable :show) '.html)))
 
 (defun /wiki#create ()
+  (unless (session-user)
+    (http-error "403 Forbidden" "Access forbidden"))
   (with-form-data (article)
     (let ((a (read-article (the string article))))
       (wiki-write-article a)
       (redirect-to (wiki-uri a)))))
 
 (defun /wiki#update (a)
+  (unless (session-user)
+    (http-error "403 Forbidden" "Access forbidden"))
   (with-form-data (article)
     (let ((new (read-article (the string article))))
       (wiki-delete-article a)
@@ -17,6 +22,8 @@
       (redirect-to (wiki-uri new)))))
 
 (defun /wiki#delete (a)
+  (unless (session-user)
+    (http-error "403 Forbidden" "Access forbidden"))
   (wiki-delete-article a)
   (redirect-to `(/wiki "index")))
 
@@ -24,13 +31,15 @@
   (print `(session-data ,(session-data (session))))
   (let ((article (when slug
 		   (or (wiki-read-article slug)
-		       (set-attributes {}
-				       :name slug
-				       :title (cl-ppcre:scan-to-strings
-					       "[^/]+$" slug)
-				       :author (user.name (session-user))
-				       :date (get-universal-time)
-				       :body "")))))
+		       (if (session-user)
+			   (set-attributes {}
+					   :name slug
+					   :title (cl-ppcre:scan-to-strings
+						   "[^/]+$" slug)
+					   :author (user.name (session-user))
+					   :date (get-universal-time)
+					   :body "")
+			 (http-error "404 Not found" "Wiki page not found"))))))
     (ecase *method*
       ((:GET)    (/wiki#show article))
       ((:POST)   (/wiki#create))
