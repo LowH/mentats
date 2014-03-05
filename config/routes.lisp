fd2f1c5a492ba49212f85d0121c0e39c9e207071
Thomas de Grivel <thomas@lowh.net>
2014-03-05 19:35:25 +0100

    Remove blog and competences

--- config/routes.lisp
+++ config/routes.lisp
@@ -6,18 +6,6 @@
 
 (define-assets-route "/assets{/name}{.ext}" "public/assets{/name}{.ext}")
 
-(define-route "/competence" `(/competence))
-(define-route "/competence/name:{name}" `(/competence :name ,(uri-var 'name)))
-(define-route "/competence/id:{id}" `(/competence :id ,(uri-var 'id)))
-
-(define-route "/blog" `(/blog))
-(define-route "/blog/{tags}" `(/blog :tags ,(uri-var 'tags)))
-(define-route "/blog/{year}/{month}" `(/blog :year ,(uri-var 'year)
-					     :month ,(uri-var 'month)))
-(define-route "/blog/{year}/{month}/{slug}" `(/blog :year ,(uri-var 'year)
-						    :month ,(uri-var 'month)
-						    :slug ,(uri-var 'slug)))
-
 (define-route "/account/register" '(/account/register))
 (define-route "/account/register/ok" '(/account/register/ok))
 (define-route "/account/login" '(/account/login))
