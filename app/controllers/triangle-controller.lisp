7c9df2536b01be919cf008bc2540b4a0fb8f3280
Thomas de Grivel <thomas@lowh.net>
2014-03-19 12:34:21 +0100

    > load-app

--- app/controllers/triangle-controller.lisp
+++ app/controllers/triangle-controller.lisp
@@ -1,20 +1,21 @@
 (defun /> (&optional command &rest args)
-  (let ((available-commands '(load-facts reload)))
+  (let ((available-commands '(load-app load-facts reload)))
     (when command
       (unless (setq command (find command available-commands
 				  :test #'string-equal))
 	#1=(http-error "404 Not Found" "Command not found")))
-    (case *method*
-      (:GET (if command
-		(template-let (command)
-		  (render-view :> :show '.html))
-		(template-let (available-commands)
-		  (render-view :> :index '.html))))
-      (:POST (unless (session-user)
-	       (http-error "403 Forbidden" "Not authorized"))
-	     (if command
-		 (render-text
-		  (with-output-to-string (*standard-output*)
-		    (apply command args)))
-		 #1#))
-      (otherwise #1#))))
+    (template-let (command available-commands)
+      (case *method*
+	(:GET (if command
+		  (template-let (command)
+		    (render-view :> :show '.html))
+		  (template-let (available-commands)
+		    (render-view :> :index '.html))))
+	(:POST (unless (session-user)
+		 (http-error "403 Forbidden" "Not authorized"))
+	       (if command
+		   (render-text
+		    (with-output-to-string (*standard-output*)
+		      (print (apply command args))))
+		   #1#))
+	(otherwise #1#)))))
