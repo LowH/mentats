b40e9efb917fab1c939d4b8ee76d1783ff8fccc1
Thomas de Grivel <thomas@lowh.net>
2014-06-24 13:56:52 +0200

    Stub for edit and trash resource front-end code.

---
+++ app/assets/js/backbone/view/mentats.resource.js
@@ -0,0 +1,10 @@
+
+Mentats.editResource = function (node) {
+  var resource = $(node).parents('.mentats-resource');
+  console.log('Mentats.editResource', resource);
+};
+
+Mentats.trashResource = function (node) {
+  var resource = $(node).parents('.mentats-resource');
+  console.log('Mentats.trashResource', resource);
+};
