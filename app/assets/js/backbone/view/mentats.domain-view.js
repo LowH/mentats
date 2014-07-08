f2921c5401796cdede9a8ee5702affcd98f533f4
Thomas de Grivel <thomas@lowh.net>
2014-07-08 14:52:10 +0200

    Fix duplicate graphs.

--- app/assets/js/backbone/view/mentats.domain-view.js
+++ app/assets/js/backbone/view/mentats.domain-view.js
@@ -4,12 +4,6 @@ Mentats.DomainView = Backbone.View.extend({
   initialize: function(options) {
     Backbone.View.prototype.initialize.apply(this, arguments);
     console.log('new Mentats.DomainView', this);
-    this.competenceGraphView = new Mentats.CompetenceGraphView({
-      model: this.model.get('competences'),
-      el: '#domain-graph-view',
-      width: 900,
-      height: 602,
-    });
   },
 
 });
--- app/assets/js/backbone/view/mentats.module-view.js
+++ app/assets/js/backbone/view/mentats.module-view.js
@@ -4,12 +4,6 @@ Mentats.ModuleView = Backbone.View.extend({
   initialize: function(options) {
     Backbone.View.prototype.initialize.apply(this, arguments);
     console.log('new Mentats.ModuleView', this);
-    this.domainsGraphView = new Mentats.DomainsGraphView({
-      model: this.model.get('domains'),
-      el: '#module-graph-view',
-      width: 900,
-      height: 602,
-    });
   },
 
 });
