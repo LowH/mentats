c54c5c5a3ca9ed707ca6bfdb7cb01efa1a08165d
Thomas de Grivel <thomas@lowh.net>
2014-07-08 15:23:02 +0200

    Competence view with module and domain thumbs

--- app/controllers/competences-controller.lisp
+++ app/controllers/competences-controller.lisp
@@ -2,13 +2,17 @@
 (defun /competence#show (competence)
   (check-can :view competence)
   (facts:with-transaction
-    (template-let (competence
-		   (resources nil))
-      (facts:with ((?r :is-a 'resource
-		       'resource.competence competence))
-	(unless (resource.deleted ?r)
-	  (push ?r resources)))
-      (render-view :competence :show '.html))))
+    (let* ((domain (competence.domain competence))
+	   (module (domain.module domain)))
+      (template-let (module
+		     domain
+		     competence
+		     (resources nil))
+	(facts:with ((?r :is-a 'resource
+			 'resource.competence competence))
+	  (unless (resource.deleted ?r)
+	    (push ?r resources)))
+	(render-view :competence :show '.html)))))
 
 (defun /competence#json (competence)
   (check-can :view competence)
@@ -31,24 +35,6 @@
 			`(,var (cdr (assoc ,key ,g!alist)))))
 		    bindings)
 	 ,@body))))
-
-(defun /competence#update (c)
-  (with-form-data (name cells)
-    (declare (type string name))
-    (when (emptyp name)
-      (http-error "400 Invalid request" "Bad input"))
-    (facts:with-transaction
-      (setf (competence.name c) name)
-      (setf (competence.cells c) cells))
-    (if (accept-p :application/json)
-	(render-json (competence.json c))
-	(redirect-to `(/competence :name ,name)))))
-
-(defun /competence#delete (c)
-  (facts:rm ((c ?p ?o)))
-  (facts:rm ((?s c ?o)))
-  (facts:rm ((?s ?p c)))
-  (redirect-to `(/competence)))
 
 (defun /competence (&optional id action)
   (let ((c (when id
