5e47a7385ceab2f4128eea30e2a7b965a4034987
Thomas de Grivel <thomas@lowh.net>
2014-09-15 15:02:44 +0200

    Add Backbone.Model.bindCollection.

--- app/assets/js/backbone/create-cache.js
+++ app/assets/js/backbone/create-cache.js
@@ -13,3 +13,22 @@ Backbone.createCache = function (model, collection) {
     return obj;
   };
 };
+
+Backbone.Model.prototype.bindCollection = function (attribute, collectionType, collectionName) {
+  collectionType = collectionType || Backbone.Collection;
+  collectionName = collectionName || attribute;
+  var col = new collectionType(_.map(this.get(attribute),
+				     collectionType.prototype.model.find));
+  var onCollectionChange = function () {
+    this.set(attribute, col.pluck('id'));
+  };
+  this.listenTo(col, 'add', onCollectionChange);
+  this.listenTo(col, 'remove', onCollectionChange);
+  this.listenTo(col, 'reset', onCollectionChange);
+  this.on('sync', function () {
+    col.set(_.map(this.get(attribute),
+		  collectionType.prototype.model.find));
+  });
+  this.collection = this.collection || {};
+  this.collection[collectionName] = col;
+};
