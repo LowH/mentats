ee3bc0d6b7da23f403b40d751fe53c36908a4776
Thomas de Grivel <thomas@lowh.net>
2014-10-28 14:39:39 +0100

    Fix editing classroom name and level.

--- app/assets/js/backbone/view/mentats.classroom-edit-view.js
+++ app/assets/js/backbone/view/mentats.classroom-edit-view.js
@@ -2,12 +2,17 @@
 Mentats.ClassroomEditView = Backbone.View.extend({
 
   events: {
-    'hide.bs.modal #classroom-modules-selector': 'onHideModulesSelector'
+    'hide.bs.modal #classroom-modules-selector': 'onHideModulesSelector',
+    'submit form': 'onSubmit'
   },
 
   initialize: function(options) {
     Backbone.View.prototype.initialize.apply(this, arguments);
     console.log('new Mentats.ClassroomEditView', this);
+
+    this.listenTo(this.model, 'change:name', this.onChangeName);
+    this.listenTo(this.model, 'change:level', this.onChangeLevel);
+
     var modules = this.model.get('modules');
     this.listenTo(modules, 'add', this.onModuleAdd);
     this.listenTo(modules, 'remove', this.onModuleRemove);
@@ -21,6 +26,14 @@ Mentats.ClassroomEditView = Backbone.View.extend({
 	selected: modules
       }
     }).render();
+  },
+
+  onChangeName: function () {
+    this.$('.classroom-name').text(this.model.get('name'));
+  },
+
+  onChangeLevel: function () {
+    this.$('.classroom-level').text(this.model.get('level'));
   },
 
   onHideModulesSelector: function (modules) {
@@ -39,6 +52,16 @@ Mentats.ClassroomEditView = Backbone.View.extend({
   onModuleRemove: function (module) {
     console.log('Mentats.ClassroomEditView.onModuleRemove', this);
     this.modulesList.children('.module.thumbnail[data-module="'+module.id+'"]').remove();
+  },
+
+  onSubmit: function (evt) {
+    console.log('Mentats.ClassroomEditView.onSubmit', this, evt);
+    this.model.set({
+      name: this.$('input[name="name"]').val(),
+      level: this.$('input[name="level"]').val()
+    });
+    this.model.save();
+    evt.preventDefault();
   }
 
 });
