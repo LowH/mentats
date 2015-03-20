2ef64248de1df25f623a9111312476c11e6e73f1
Thomas de Grivel <thomas@lowh.net>
2015-03-20 15:46:26 +0100

    Update can permissions.

--- config/permissions.lisp
+++ config/permissions.lisp
@@ -3,21 +3,33 @@
 
 (can:define-permission (:everyone :can :admin :all))
 (can:define-permission (:anonymous :cannot :admin :all))
-(can:define-permission (:anonymous :can :view :all))
+;(can:define-permission (:anonymous :can :view :all))
 (can:define-permission (:anonymous :can :list 'modules))
 
+(can:define-permission (:anonymous :can :view ?article)
+  (and (typep ?article 'json:fluid-object)
+       (not (article-has-tag :private ?article))))
+
+(can:define-permission (:anonymous :can :view ?domain)
+  (and (facts:bound-p ((?domain :is-a 'domain)))
+       (not (domain.deleted ?domain))))
+
+(can:define-permission (:anonymous :can :view ?module)
+  (and (facts:bound-p ((?module :is-a 'module)))
+       (not (module.deleted ?module))))
+
 (can:define-permission (?user :can :edit ?module)
-  (?user :is-a 'user)
-  (?module :is-a 'module
-	   'module.owner ?user))
+  (facts:bound-p ((?user :is-a 'user)
+                  (?module :is-a 'module
+                           'module.owner ?user))))
 
 (can:define-permission (?user :can :edit ?domain)
-  (?user :is-a 'user)
-  (?domain 'domain.module ?module)
-  (?module 'module.owner ?user))
+  (facts:bound-p ((?user :is-a 'user)
+                  (?domain 'domain.module ?module)
+                  (?module 'module.owner ?user))))
 
 (can:define-permission (?user :can :admin :all)
-  (?user :is-a 'user
-	 'user.email "thomas@lowh.net"))
+  (facts:bound-p ((?user :is-a 'user
+                         'user.email "thomas@lowh.net"))))
 
 (can:compile-rules)
