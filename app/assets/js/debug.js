e9c2ac2a7c8dbec05e3a22b450cbc3fb3de44eaf
Thomas de Grivel <thomas@lowh.net>
2015-04-04 19:33:45 +0200

    Fix debug.js

--- app/assets/js/debug.js
+++ app/assets/js/debug.js
@@ -6,31 +6,37 @@ _.extend(debug, {
   all: false,
 
   disable: function (tags) {
-    var t = (typeof tags == 'string') ? arguments : tags;
-    this._tags = _.partial(_.without, this._tags).apply(t);
+    var t = _.isArray(tags) ? tags : arguments;
+    this._tags = _.partial(_.without, this._tags)(t);
     return this;
   },
 
   enable: function (tags) {
-    var t = (typeof tags == 'string') ? arguments : tags;
-    this._tags = _.partial(_.union, this._tags).apply(t);
+    var t = _.isArray(tags) ? tags : arguments;
+    this._tags = _.partial(_.union, this._tags)(t);
     return this;
   },
 
   tag: function (tags) {
-    var t = (typeof tags == 'string') ? arguments : tags;
+    var t = _.isArray(tags) ? tags : arguments;
     if (this.all)
       return t[0];
-    return _.find(t, function (x) {
-      return _.find(this._tags, x);
-    });
+    var i;
+    for (i = 0; i < t.length; i++) {
+      var j;
+      for (j = 0; j < this._tags.length; j++)
+        if (t[i] == this._tags[j])
+          return t[i];
+    }
+    return undefined;
   },
 
   _tags: [],
 
   tags: function (tags) {
-    var t = (typeof tags == 'string') ? arguments : tags;
-    return _.intersection(this._tags, t);
+    var t = _.isArray(tags) ? tags : arguments;
+    var i = _.intersection(this._tags, t);
+    return i.length == 0 ? false : i;
   },
 
   log: function (tags, msg) {
